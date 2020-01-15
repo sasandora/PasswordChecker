c9c4bd2a3e938e1bffaf870587a9b0d10dff6412
sasandora <kalma5663@gmail.com>
2020-01-15 15:27:42 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PasswordChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lblHash = new System.Windows.Forms.Label();
+            this.lblResponse = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(36, 76);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(330, 20);
+            this.txtPass.TabIndex = 0;
+            this.txtPass.TextChanged += new System.EventHandler(this.TextToHash);
+            this.txtPass.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPass_KeyDown);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(406, 76);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Check";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lblHash
+            // 
+            this.lblHash.AccessibleRole = System.Windows.Forms.AccessibleRole.Text;
+            this.lblHash.AutoSize = true;
+            this.lblHash.Location = new System.Drawing.Point(33, 124);
+            this.lblHash.Name = "lblHash";
+            this.lblHash.Size = new System.Drawing.Size(35, 13);
+            this.lblHash.TabIndex = 2;
+            this.lblHash.Text = "Hash:";
+            // 
+            // lblResponse
+            // 
+            this.lblResponse.AutoSize = true;
+            this.lblResponse.Location = new System.Drawing.Point(33, 178);
+            this.lblResponse.Name = "lblResponse";
+            this.lblResponse.Size = new System.Drawing.Size(0, 13);
+            this.lblResponse.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 318);
+            this.Controls.Add(this.lblResponse);
+            this.Controls.Add(this.lblHash);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtPass);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lblHash;
+        private System.Windows.Forms.Label lblResponse;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PasswordChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static readonly HttpClient client = new HttpClient();
+
+        private void TextToHash(object sender, EventArgs e)
+        {
+            string source = txtPass.Text.ToString();            // Käyttäjän syöttämä teksti
+            string hash = Hashing(source);
+            lblHash.Text = "Hash: " + hash + "\n5 ensimmäistä kirjainta: " + hash.Remove(5);
+        }
+
+        private static string Hashing(string source)
+        {
+            SHA1 sha = new SHA1CryptoServiceProvider();         // Hash-algoritmi
+            string hash = GetHash(sha, source);                 // Viedään teksti tiivistettäväksi
+            return hash;                                        // Palautetaan tiiviste
+
+            string GetHash(HashAlgorithm hashAlgorithm, string input)
+            {
+                byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input)); // Luo syötetystä tekstistä bittijonon ja laskee siitä tiivisteen
+                var sBuilder = new StringBuilder();                                     // StringBuilder bittijonon muuttamiseksi takaisin stringiksi
+
+                for (int i = 0; i < data.Length; i++) {                                 // Käydään läpi jokainen tavu ja muutetaan se heksadesimaaliksi
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+                return sBuilder.ToString();                                             // Palautetaan heksadesimaali
+            }
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            string source = txtPass.Text.ToString();            // Käyttäjän syöttämä teksti
+            string hash = Hashing(source);
+            string hash2 = Hashing(source);
+
+            if(hash == hash2) {
+                var response = await SendHash(hash);                // Käydään tietokannasta vertailutulokset
+                string r = response.ToString();                     // Muutetaan tulos stringiksi
+                hash = hash.Substring(5).ToUpper();                 // Vastaus tulee isoilla kirjaimilla, joten tiiviste myös isoksi
+                                                                    // Lisäksi otetaan tiivisteen viisi ensimmäistä kirjainta pois, sillä ne on poistettu myös  vastauksesta
+
+                lblResponse.Text = CompareResponseToHash(hash, r);  // Verrataan alkuperäistä tiivistettä tulokseen
+            }
+            else {
+                lblResponse.Text = "Tiivisteen varmennus epäonnistui";
+            }
+        }
+
+        private static async Task<String> SendHash(string hash)
+        {
+            try {
+                string getRequest = "https://api.pwnedpasswords.com/range/" + hash.Remove(5);   // Luodaan URI tiivisteen 5:stä ensimmäisestä kirjaimesta
+                var response = await client.GetStringAsync(getRequest);                         // Käydään vertaamassa tietokantaan
+                return response;
+            } catch (HttpRequestException e) {
+                return null;
+            }
+        }
+
+        private static string CompareResponseToHash(string hash, string response)
+        {
+            StringReader stReader = new StringReader(response);     // Luetaan vastaus rivi kerrallaan
+
+            string aLine = stReader.ReadLine();
+            while (aLine != null) {
+                string[] jako = aLine.Split(':');   // Jaetaan rivi kahteen osaan.
+
+                if (jako[0] == hash) {
+                    return "Salanasi löytyi " + jako[1] + " kertaa";
+                }
+                aLine = stReader.ReadLine();
+            }
+            return "Salasanaa ei löytynyt julkistetuista vuodoista";
+        }
+
+        private void txtPass_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Enter)) {
+                btnSend_Click(sender, e);
+                e.SuppressKeyPress = true;  // Hiljentää *ding* äänen
+            }
+        }
+    }
+}
